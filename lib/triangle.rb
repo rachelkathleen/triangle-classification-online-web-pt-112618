f563851bf23a98ca60faac2ea5d8fa942942c240
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-14 03:31:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,13 +13,15 @@ class Triangle
       :equilateral
     elsif a == b || b == c || a == c
       :isosceles
-    else 
+    else
       :scalene
     end
   end
 
   def is_a_triangle
-    triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    triangle = [(a + b > c), (a + c > b), (b + c > a), (a > 0), (b > 0), (c > 0)]
+    raise  TriangleError if triangle.include?(false)
+  end
 
   class TriangleError < StandardError
   end
