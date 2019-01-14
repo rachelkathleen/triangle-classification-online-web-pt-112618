72e645d5bbae6db4e8d339e19646d5204ab3a426
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-14 03:26:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,11 @@
 class Triangle
-  attr_accessor :side_one, :side_two, :side_three
+  attr_accessor :a, :b, :c
 
-  def initialize (side_one, side_two, side_three)
-    @side_one = side_one
-    @side_two = side_two
-    @side_three = side_three
-  end
+  def initialize(a, b, c)
+     @a = a
+     @b = b
+     @c = c
+   end
 
   def kind
     if side_one + side_two =< side_three || side_one + side_three =< side_two || side_two + side_three =< side_two || side_one > 0 || side_two > 0 || side_three > 0
