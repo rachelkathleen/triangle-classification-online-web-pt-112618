0523bcb70aa3e2f17d1c4fec7ffefa4b6b47ac3f
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-14 03:22:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,8 +8,8 @@ class Triangle
   end
 
   def kind
-    if side_one + side_two !> side_three || side_one + side_three !> side_two || side_two + side_three !> side_two || 
-
+    if side_one + side_two !> side_three || side_one + side_three !> side_two || side_two + side_three !> side_two || side_one > 0 || side_two > 0 || side_three > 0
+      raise TriangleError
     elsif side_one == side_two && side_two == side_three && side_one == side_three
       :equilateral
     elsif side_one != side_two && side_one != side_three && side_two != side_three
